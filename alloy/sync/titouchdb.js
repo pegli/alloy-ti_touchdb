6a5c3646aeef6a074a2723deeab0f8da4212bec3
Paul Mietz Egli <paul@obscure.com>
2012-11-05 10:00:47 -0800

    bug fix: fixed problem where default view was used for collections

--- alloy/sync/titouchdb.js
+++ alloy/sync/titouchdb.js
@@ -85,7 +85,7 @@ function Sync(model, method, options) {
         
         // collection
         var ddoc = collection.config.adapter.collection_name;
-        var view = opts.view || collection.config.adapter.views[0];
+        var view = opts.view || collection.config.adapter.views[0].name;
         
         // add default view options from model
         opts = _.defaults(opts, collection.config.adapter.view_options);
